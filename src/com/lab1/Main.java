c566b1a169596f4a69ca22213ffabb64d8a6d7ce
MrBoggins <onofrei.grigore2001@gmail.com>
2022-02-26 16:57:51 +0200

    v1.1

--- src/com/lab1/Main.java
+++ src/com/lab1/Main.java
@@ -1,12 +1,12 @@
 package com.lab1;
-import java.io.*;
+//import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException
+    public static void main(String[] args)
     {
 	    System.out.println("ohayo!");
-        BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedReader b = new BufferedReader(new InputStreamReader(System.in));
         //int a = Integer.parseInt(b.readLine());
     }
 }
