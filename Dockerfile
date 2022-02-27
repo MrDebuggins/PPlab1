8388565e78639ae463cb5bb39457cee9958a75f7
MrBoggins <onofrei.grigore2001@gmail.com>
2022-02-27 20:19:46 +0200

    v2.0

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM openjdk:11
 RUN git clone https://github.com/MrDebuggins/PPlab1.git
 WORKDIR /PPlab1
 CMD java -cp out/production/untitled104 com.lab1.Main
-RUN dir
--- src/com/lab1/Main.java
+++ src/com/lab1/Main.java
@@ -1,27 +1,68 @@
 package com.lab1;
+import java.io.*;
+import java.util.Iterator;
 import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.ArrayList;
 
-public class Main {
-
+public class Main
+{
     public static void main(String[] args) throws IOException
     {
+        ArrayList<String> stringBuilder = new ArrayList<>();
+
 	    System.out.println("Start running...");
         try
         {
-            File obj = new File("text.txt");
-            //obj.createNewFile();
-            Scanner scan = new Scanner(obj);
-            String str = scan.nextLine();
-            System.out.println(str);
-            scan = new Scanner(System.in);
-            scan.nextLine();
-        }catch (FileNotFoundException e)
+            FileReader read = new FileReader("text.txt");
+            Scanner scan = new Scanner(read);
+
+            System.out.println("Original file: ");
+            String str;
+            while(scan.hasNextLine())
+            {
+                try
+                {
+                    str = scan.nextLine();
+                }catch (Exception e)
+                {
+                    break;
+                }
+
+                stringBuilder.add(str);
+                System.out.println(str);
+            }
+            scan.close();
+
+            for(int i = 0; i < stringBuilder.size(); i++)
+            {
+                str = stringBuilder.get(i);
+
+                str = str.replace(".", "");
+                str = str.replace(",", "");
+                str = str.replace("?", "");
+                str = str.replace("!", "");
+                str = str.replace("(", "");
+                str = str.replace(")",  "");
+
+                str = str.replace("  ", " ");
+                str = str.replace("a", "A");
+
+                stringBuilder.set(i, str);
+            }
+
+            FileWriter write = new FileWriter("text.txt");
+            System.out.println("\nEditted file:");
+            for(int i = 0; i < stringBuilder.size(); i++)
+            {
+                str = stringBuilder.get(i);
+                System.out.println(str);
+                write.write(str + "\n");
+            }
+            write.close();
+        }
+        catch (FileNotFoundException e)
         {
             System.out.println("File not found!");
         }
-
     }
 }
