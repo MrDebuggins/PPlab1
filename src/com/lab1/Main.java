0caa34f0d06a86c5789a687acb3f7569acb5bc2f
MrBoggins <onofrei.grigore2001@gmail.com>
2022-02-27 13:28:36 +0200

    v1.2

--- src/com/lab1/Main.java
+++ src/com/lab1/Main.java
@@ -1,9 +1,14 @@
 package com.lab1;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args)
     {
 	    System.out.println("ohayo!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("input: ");
+        int i = scan.nextInt();
+        System.out.println("i = " + i);
     }
 }
